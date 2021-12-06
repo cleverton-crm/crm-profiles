89df5567598d8a01d50781bc4dd24e5c3ef06f75
Alexander Hunter <icehuntmen@gmail.com>
2021-12-06 20:58:19 +0300

    Add fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ProfileModule } from './profile.module';
+import { UserModule } from '../../users/src/user.module';
+import { Logger } from 'mongodb';
+import { TcpOptions, Transport } from '@nestjs/microservices';
+import { cyan } from 'cli-color';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ProfileModule);
-  //test
-  await app.listen(3000);
+  const logger = new Logger('ProfileModule');
+  const PORT = process.env.PROFILE_SERVICE_PORT || 4002;
+  const app = await NestFactory.createMicroservice(UserModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: PORT,
+    },
+  } as TcpOptions);
+
+  await app.listen();
 }
 bootstrap();
