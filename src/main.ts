f2209154f51765996f9aba2803cf1d41e3939ddb
Roman Cold <colddevelop@gmail.com>
2022-02-01 15:31:14 +0300

    Graceful shutdown для модуля

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ProfileModule } from './profile.module';
 import { TcpOptions, Transport } from '@nestjs/microservices';
 import { cyan } from 'cli-color';
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { ConfigService } from './services';
 
 async function bootstrap() {
@@ -24,3 +24,25 @@ async function bootstrap() {
   await app.listen();
 }
 bootstrap();
+
+
+let app: INestApplication;
+const logger = new Logger('NestApplication');
+
+async function gracefulShutdown(): Promise<void> {
+  if (app !== undefined) {
+    await app.close();
+    logger.warn('Application closed!');
+  }
+  process.exit(0);
+}
+
+process.once('SIGTERM', async () => {
+  logger.error('SIGTERM: Graceful shutdown... ');
+  await gracefulShutdown();
+});
+
+process.once('SIGINT', async () => {
+  logger.error('SIGINT: Graceful shutdown... ');
+  await gracefulShutdown();
+});
